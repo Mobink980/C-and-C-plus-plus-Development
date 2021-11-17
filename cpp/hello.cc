eec7c6e9f01cf2353f5cc3279774fc6b7d900af9
Mobink980 <mobink980@gmail.com>
2021-11-17 21:00:28 +0330

    some files

---
+++ cpp/hello.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    //commenting is fun!
+    cout << "Hello, World!\n";
+    cout << "Bonjour du le monde!\n";
+
+    /* Ha Ha Ha
+     * This is a multi-line comment
+     * programming is fun!
+     */
+
+    //defining an integer
+    int x = -5; //initializing can be also like x(-5) or x {-5}!
+    int height { 6 }; //brace initialization is preferrred to avoid data loss
+   
+    return 0;
+}
